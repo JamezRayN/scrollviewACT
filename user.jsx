40fd22f04df4dac7aac909ffb1d6a584c564184a
JamezRayN <james.noble980@gmail.com>
2023-11-18 17:27:38 +0800

    1UP

---
+++ user.jsx
@@ -0,0 +1,83 @@
+export const users = [
+    {
+      id: 1,
+      name: {
+        firstname: "JUNREY",
+        middlename: "",
+        lastname: "GENZON",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 2,
+      name: {
+        firstname: "PETER KYLE",
+        middlename: "",
+        lastname: "GINGO",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 3,
+      name: {
+        firstname: "MARK ALDWIN",
+        middlename: "",
+        lastname: "HANDUMON",
+      },
+      address: "Paris, France",
+    },
+    {
+      id: 4,
+      name: {
+        firstname: "RODEL GLENN",
+        middlename: "",
+        lastname: "JAWA",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 5,
+      name: {
+        firstname: "JOHN MARK",
+        middlename: "",
+        lastname: "LAGRIMAS",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 6,
+      name: {
+        firstname: "JOHN CARLO",
+        middlename: "",
+        lastname: "NAMOR",
+      },
+      address: "Las Vegas, Nevada",
+    },
+    {
+      id: 7,
+      name: {
+        firstname: "JOHN CARLO",
+        middlename: "",
+        lastname: "RAZON",
+      },
+      address: "Paris, France",
+    },
+    {
+      id: 8,
+      name: {
+        firstname: "JESS RAPHAEL",
+        middlename: "",
+        lastname: "JUMAWAN",
+      },
+      address: "Los Angeles, California",
+    },
+    {
+      id: 9,
+      name: {
+        firstname: "JAMES RAYMOND",
+        middlename: "",
+        lastname: "NOBLE",
+      },
+      address: "Los Angeles, California",
+    },
+  ];
